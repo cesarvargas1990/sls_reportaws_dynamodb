056ab95d8665d645c512f391e30726ac7d04a0be
cesarvargas1990 <ceavarga@bancolombia.com.co>
2021-11-10 12:25:53 -0500

    adjust validation

--- model_validation/reportModel.js
+++ model_validation/reportModel.js
@@ -1,14 +1,12 @@
-/* eslint @typescript-eslint/no-var-requires: "off" */
-
 const Joi = require('joi');
 
 const reportModel = Joi.object().keys({ 
-    name: Joi.string().min(3).max(30).required(),
-    alias: Joi.string().min(3).max(30).required(),
-    species: Joi.string().min(3).max(30).required(),
+    name: Joi.string().min(3).max(25).required(),
+    alias: Joi.string().min(3).max(25).required(),
+    species: Joi.string().min(3).max(25).required(),
     company: Joi.object().required().keys({
-        name: Joi.string().min(3).max(30).required(),
-        team: Joi.string().min(3).max(30).required()
+        name: Joi.string().min(3).max(25).required(),
+        team: Joi.string().min(3).max(25).required()
     })
 }); 
 
